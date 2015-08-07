7dbb0cb5138d48bd8d5aba934db3377ef2d911d4
Saghm Rossi <saghmrossi@gmail.com>
2015-08-07 18:04:24 -0400

    added basic export functionality

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,23 @@
+extern crate bson;
 extern crate docopt;
+extern crate mongodb;
+extern crate rustc_serialize;
 
-#[macro_use]
-mod version;
+#[macro_use] mod version;
 mod export;
 
 use docopt::Docopt;
+use export::Exporter;
 
 static USAGE : &'static str = "
-USAGE: tariff (-e | -i) <file>
+USAGE: tariff (-e | -i) <file> --db <db> --coll <coll>
        tariff (--help | --version)
 
 Options:
   -e, --export    Exports database to file.
   -i, --import    Imports database from file.
+  -d, --db        Which database to import/export.
+  -c, --coll      Which collection to import/export.
   -h, --help      Show this message.
   -v, --version   Show the version of tariff.
 ";
@@ -23,11 +28,21 @@ fn main() {
 
   if args.get_bool("--version") {
       println!("{}", version!());
+      return;
   }
 
   let file = args.get_str("<file>");
+  let mut exporter = match Exporter::new() {
+      Ok(exporter) => exporter,
+      Err(e) => panic!("Error: {}", e)
+  };
+
+  let db = args.get_str("<db>");
+  let coll = args.get_str("<coll>");
 
   if args.get_bool("--export") {
-      export::export(file);
+      if let Err(e) = exporter.dump(db, coll, file) {
+          panic!("Error: {}", e);
+      }
   }
 }
